6b65e3df3524d491189c1d488b7d47aff3bd9452
Johnny Cakes <biden@phat.co.uk>
2025-11-07 16:39:19 -0600

    merge

--- src/aquifer/research/main.py
+++ src/aquifer/research/main.py
@@ -6,14 +6,14 @@ from perplexity_query.main import research_query_with_perplexity
 
 query = Path("query_string.jinja2").read_text()
 
-if not Path("response_exa.txt").exists():
-    response_exa = research_query_with_exa(query)
-    with open("response_exa.txt", "w") as f:
-        f.write(response_exa)
 if not Path("response_openai.txt").exists():
     response_openai = research_query_with_openai(query)
     with open("response_openai.txt", "w") as f:
         f.write(response_openai)
+if not Path("response_exa.txt").exists():
+    response_exa = research_query_with_exa(query)
+    with open("response_exa.txt", "w") as f:
+        f.write(response_exa)
 if not Path("response_perplexity.txt").exists():
     response_perplexity = research_query_with_perplexity(query)
     with open("response_perplexity.txt", "w") as f:
